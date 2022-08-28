c5819b4c9747d4b0041931e661841c24a30b6285
axzed <waxwc1491224820@gmail.com>
2022-08-28 10:13:09 +0800

    [FEAT] 创建项目，完成配置初始化

---
+++ dao/mysql/mysql.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"bluebell/settings"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+var db *sqlx.DB
+
+func Init(cfg *settings.MySQLConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DbName,
+	)
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		zap.L().Error("connect DB failed, err:%v\n", zap.Error(err))
+		return
+	}
+	db.SetMaxOpenConns(cfg.MaxOpenConnection)
+	db.SetMaxIdleConns(cfg.MaxIdleConnection)
+	return
+}
+
+// 对外封装一个Close方法
+func Close() {
+	_ = db.Close()
+}
